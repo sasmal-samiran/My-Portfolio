f9aa255ad5eb147d453832facce1357e9e397fd5
Samiran Sasmal <sasmalsamiran7557@gmail.com>
2025-08-01 08:15:25 +0530

    New Project Added

--- TravelPlanner/routes.py
+++ TravelPlanner/routes.py
@@ -3,8 +3,8 @@ import TravelPlanner.services as services
 
 travelplanner = Blueprint('travelplanner', 
     __name__, 
-    template_folder='TravelPlanner/templates', 
-    static_folder='TravelPlanner/static',  
+    template_folder='templates', 
+    static_folder='static',  
     url_prefix='/travelplanner')
 
 @travelplanner.route('/')
