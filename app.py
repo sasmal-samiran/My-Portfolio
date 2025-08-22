cd2ebc1af9589ac3da16acdd2e7fe1f8a2bef2ac
Samiran Sasmal <sasmalsamiran7557@gmail.com>
2025-08-23 02:40:59 +0530

    Updated security features

--- app.py
+++ app.py
@@ -71,95 +71,95 @@ app.config['SECRET_KEY'] = os.urandom(24)
 # Setup logging
 app.logger.setLevel(logging.INFO)
 
-def is_registered():
-    return request.cookies.get("registered-username") and request.cookies.get("registered-userpassword")
+# def is_registered():
+#     return request.cookies.get("registered-username") and request.cookies.get("registered-userpassword")
 
-def is_logged_in():
-    return request.cookies.get("login-username") and request.cookies.get("login-userpassword")
+# def is_logged_in():
+#     return request.cookies.get("login-username") and request.cookies.get("login-userpassword")
 
 @app.route('/')
 def index():
     tech_stack = ['Python', 'Flask', 'JavaScript', 'HTML5', 'CSS3', 'PostgreSQL', 'Docker']
     alertMessage = request.args.get('alertMessage')
-    alreadyRegistered = is_registered()
-    return render_template('index.html', alertMessage=alertMessage, tech_stack=tech_stack, alreadyRegistered=alreadyRegistered)
+    # alreadyRegistered = is_registered()
+    return render_template('index.html', alertMessage=alertMessage, tech_stack=tech_stack) #alreadyRegistered=alreadyRegistered)
 
-@app.route('/login')
-def login():
-    alertMessage = request.args.get('alertMessage')
-    next_page = request.args.get('next')
-    return render_template('login.html', alertMessage=alertMessage, next=next_page)
+# @app.route('/login')
+# def login():
+#     alertMessage = request.args.get('alertMessage')
+#     next_page = request.args.get('next')
+#     return render_template('login.html', alertMessage=alertMessage, next=next_page)
 
-@app.route('/thank/<name>')
-def thank(name):
-    message = request.args.get('message')
-    return render_template("thank.html", name=name, message=message)
+# @app.route('/thank/<name>')
+# def thank(name):
+#     message = request.args.get('message')
+#     return render_template("thank.html", name=name, message=message)
 
 @app.route('/register')
 def register():
     alertMessage = request.args.get('alertMessage')
     return render_template("register.html", alertMessage=alertMessage)
 
-@app.route('/register/registered', methods=['POST'])
-def registered():
-    if not is_registered():
-        userFirstName = request.form.get("userfirstname").strip()
-        userLastName = request.form.get("userlastname").strip()
-        userName = f"{userFirstName} {userLastName}"
-        email = request.form.get("useremail").strip()
-        password = request.form.get("userpassword").strip()
+# @app.route('/register/registered', methods=['POST'])
+# def registered():
+#     if not is_registered():
+#         userFirstName = request.form.get("userfirstname").strip()
+#         userLastName = request.form.get("userlastname").strip()
+#         userName = f"{userFirstName} {userLastName}"
+#         email = request.form.get("useremail").strip()
+#         password = request.form.get("userpassword").strip()
 
-        resp = make_response(redirect(url_for('thank', name=userName, message='Registering')))
-        max_age = 60 * 60 * 24 * 365 * 20
-        resp.set_cookie("registered-username", userName.lower(), max_age=max_age)
-        resp.set_cookie("registered-useremail", email, max_age=max_age)
-        resp.set_cookie("registered-userpassword", password, max_age=max_age)
-        return resp
-    flash("You already have an Active Account")
-    return redirect(url_for('login'))
+#         resp = make_response(redirect(url_for('thank', name=userName, message='Registering')))
+#         max_age = 60 * 60 * 24 * 365 * 20
+#         resp.set_cookie("registered-username", userName.lower(), max_age=max_age)
+#         resp.set_cookie("registered-useremail", email, max_age=max_age)
+#         resp.set_cookie("registered-userpassword", password, max_age=max_age)
+#         return resp
+#     flash("You already have an Active Account")
+#     return redirect(url_for('login'))
 
-@app.route('/login/logged', methods=['POST'])
-def logged():
-    if is_registered():
-        name = request.form.get("username").strip().lower()
-        email = request.form.get("useremail").strip()
-        password = request.form.get("userpassword").strip()
-        remember = request.form.get("remember")
-        next_page = request.form.get('next') or url_for('index')
+# @app.route('/login/logged', methods=['POST'])
+# def logged():
+#     if is_registered():
+#         name = request.form.get("username").strip().lower()
+#         email = request.form.get("useremail").strip()
+#         password = request.form.get("userpassword").strip()
+#         remember = request.form.get("remember")
+#         next_page = request.form.get('next') or url_for('index')
 
-        registered_name = request.cookies.get("registered-username")
-        registered_email = request.cookies.get("registered-useremail")
-        registered_password = request.cookies.get("registered-userpassword")
+#         registered_name = request.cookies.get("registered-username")
+#         registered_email = request.cookies.get("registered-useremail")
+#         registered_password = request.cookies.get("registered-userpassword")
 
-        if name == registered_name and email == registered_email and password == registered_password:
-            resp = make_response(redirect(next_page))
-            if remember == "on":
-                max_age = 60 * 60
-                resp.set_cookie("login-username", name, max_age=max_age)
-                resp.set_cookie("login-useremail", email, max_age=max_age)
-                resp.set_cookie("login-userpassword", password, max_age=max_age)
-            return resp
+#         if name == registered_name and email == registered_email and password == registered_password:
+#             resp = make_response(redirect(next_page))
+#             if remember == "on":
+#                 max_age = 60 * 60
+#                 resp.set_cookie("login-username", name, max_age=max_age)
+#                 resp.set_cookie("login-useremail", email, max_age=max_age)
+#                 resp.set_cookie("login-userpassword", password, max_age=max_age)
+#             return resp
 
-        flash('Wrong Credentials!')
-        return redirect(url_for('login'))
+#         flash('Wrong Credentials!')
+#         return redirect(url_for('login'))
 
-    flash("You do not have an Active Account")
-    return redirect(url_for('register'))
+#     flash("You do not have an Active Account")
+#     return redirect(url_for('register'))
 
-@app.route('/forgetpass')
-def forgetpass():
-    if is_registered():
-        registered_name = request.cookies.get("registered-username")
-        registered_email = request.cookies.get("registered-useremail")
-        registered_password = request.cookies.get("registered-userpassword")
+# @app.route('/forgetpass')
+# def forgetpass():
+#     if is_registered():
+#         registered_name = request.cookies.get("registered-username")
+#         registered_email = request.cookies.get("registered-useremail")
+#         registered_password = request.cookies.get("registered-userpassword")
 
-        return f'''<h1> YOUR LOGIN DETAILS! </h1>
-                <h5>&emsp;Username: {registered_name}</h5>
-                <h5>&emsp;Email: {registered_email}</h5>
-                <h5>&emsp;Password: {registered_password}</h5>'''
+#         return f'''<h1> YOUR LOGIN DETAILS! </h1>
+#                 <h5>&emsp;Username: {registered_name}</h5>
+#                 <h5>&emsp;Email: {registered_email}</h5>
+#                 <h5>&emsp;Password: {registered_password}</h5>'''
     
-    flash('Please register yourself')
-    return redirect(url_for('register'))
+#     flash('Please register yourself')
+#     return redirect(url_for('register'))
 
 @app.route('/projects/')
 def projects():
